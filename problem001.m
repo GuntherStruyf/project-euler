c486d0999ac92e61edf0a4cc2353e28c797db11e
Gunther Struyf <gunther.struyf@gmail.com>
2012-10-19 15:26:55 +0200

    solved problem 002

--- problem001.m
+++ problem001.m
@@ -6,23 +6,22 @@ function y = problem001(x)
 %
 % Find the sum of all the multiples of 3 or 5 below 1000.
 
-% _below_ input number
-x=x-1;
-
-% sum of multiples of 3
-sum3 = 3*sumto(floor(x/3));
-
-% sum of multiples of 5
-% 0+5+10+15+20+... = 5*(0+1+2+3+4+...)
-sum5 = 5*sumto(floor(x/5));
-
-% overlap
-% 0 15 30 45 60 ...
-overlap = 15*sumto(floor(x/15));
-
-% total sum
-y = sum3 + sum5 - overlap;
-
+	% _below_ input number
+	x=x-1;
+	
+	% sum of multiples of 3
+	sum3 = 3*sumto(floor(x/3));
+	
+	% sum of multiples of 5
+	% 0+5+10+15+20+... = 5*(0+1+2+3+4+...)
+	sum5 = 5*sumto(floor(x/5));
+	
+	% overlap
+	% 0 15 30 45 60 ...
+	overlap = 15*sumto(floor(x/15));
+	
+	% total sum
+	y = sum3 + sum5 - overlap;
 end
 function y = sumto(x)
 	% sum(1:x)
